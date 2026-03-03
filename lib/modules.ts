28a67f2cd1866d599e058a198d6af03ad25baf40
AbelPhilippe <abelphilipe@live.com>
2026-03-03 04:24:32 -0300

    Update Dictionary

--- lib/modules.ts
+++ lib/modules.ts
@@ -134,5 +134,174 @@ export const modules: Module[] = [
         query: 'site:(site) intitle:"phpinfo()" | inurl:phpinfo | intitle:"php configuration" | inurl:"phpconf" | inurl:"php_config" | inurl:"phpinfo.php"',
       },
     ],
+  },
+  {
+    id: "10",
+    title: "Shells",
+    description: "Find potential web shells",
+    category: "BROWSER",
+    dorks: [
+      {
+        id: "10-1",
+        label: "Web Shells",
+        query: 'site:(site) inurl:c99 | inurl:r57 | i inurl:cmd | inurl:command | inurl:shell | inurl:backdoor | inurl:wso | inurl:cmd | shadow | passwd | boot.ini | inurl:backdoor | inurl:ws0 | inurl:cmd | inurl:command | inurl:exec | inurl:execute',
+      },
+    ],
+  },
+  {
+    id: "11",
+    title: "Open Redirects",
+    description: "Find potential open redirect vulnerabilities",
+    category: "BROWSER",
+    dorks: [
+      {
+        id: "11-1",
+        label: "Open Redirects",
+        query: 'site:(site) inurl:"redirect=" | inurl:"data=" | inurl:return | inurl:"reference=" | inurl:"_redir=" | inurl:"_url=" | inurl:"_next=" | inurl:"_reference=" | inurl:src=http | inurl:r=http | inurl:redirect_uri=http | inurl:redirect_url=http | inurl:return=http | inurl:return_url=http | inurl:next=http | inurl:dest=http | inurl:destination=http | inurl:url=http',
+      },
+    ],
+  },
+  {
+      id: "12",
+      title: "Structs RCEs",
+      description: "Find potential struct RCE vulnerabilities",
+      category: "BROWSER",
+      dorks: [
+        {
+          id: "12-1",
+          label: "Struct RCEs",
+          query: 'site:(site) site:test.com ext:action | ext:struts | ext:do | inurl:"?search=" | inurl:"?query=" | inurl:"?q=" | inurl:"?s=" | inurl:"?keyword=" | inurl:"?keywords=" | inurl:"?term=" | inurl:"?terms=" | inurl:"?filter=" | inurl:"?id=" | inurl:"?page=" | inurl:"?view=" | inurl:"?type="',
+        },
+      ],
+  },
+  {
+      id: "13",
+      title: "WP Admin Pages and Files",
+      description: "Find WordPress admin pages and files",
+      category: "BROWSER",
+      dorks: [
+        {
+          id: "13-1",
+          label: "WP Admin Pages",
+          query: 'site:(site) inurl:"wp-admin" | inurl:wp-content | inurl:wp-includes | inurl:wp-uploads | inurl:wp-config.php | inurl:readme.html | inurl:license.txt | inurl:xmlrpc.php | inurl:wp-json | inurl:wp-cron.php | inurl:wp-admin.php | inurl:admin-ajax.php | inurl:admin.php | inurl:login.php',
+        },
+      ],
+  },
+  {
+    id: "14",
+    title: "Exposed AWS Buckets",
+    description: "Find publicly accessible AWS S3 buckets",
+    category: "BROWSER",
+    dorks: [
+      {
+        id: "14-1",
+        label: "AWS S3 Buckets",
+        query: 'site:(site) intitle:"index of" inurl:s3.amazonaws.com | inurl:bucket | inurl:storage | inurl:files | inurl:data | inurl:backup | inurl:assets | inurl:media | inurl:downloads | inurl:uploads',
+      },
+    ],
+  },
+  {
+      id: "15",
+      title: "Exposed Git Repositories",
+      description: "Find publicly accessible Git repositories",
+      category: "BROWSER",
+      dorks: [
+        {
+          id: "15-1",
+          label: "Git Repos",
+          query: 'site:(site) inurl:".git" | inurl:"/.git" | inurl:"/git" | inurl:"/repository" | inurl:"/repo" | inurl:"/version-control" | inurl:"/source-code" | inurl:"/scm" | inurl:"/vcs" | inurl:".gitignore" -github',
+        },
+      ],
+  },
+  {
+      id: "16",
+      title: "Exposed Jenkins Instances",
+      description: "Find publicly accessible Jenkins instances",
+      category: "BROWSER",
+      dorks: [
+        {
+          id: "16-1",
+          label: "Jenkins Instances",
+          query: 'site:(site) inurl:"/jenkins" | inurl:"/hudson" | inurl:"/ci" | inurl:"/build" | inurl:"/job" | inurl:"/view" | inurl:"/queue" | inurl:"/computer" | inurl:"/node" | inurl:"/plugin" | inurl:"/script" | inurl:"/api/json"',
+        },
+      ],
+  },
+  {
+      id: "17",
+      title: "Exposed Docker Registries",
+      description: "Find publicly accessible Docker registries",
+      category: "BROWSER",
+      dorks: [
+        {
+          id: "17-1",
+          label: "Docker Registries",
+          query: 'site:(site) inurl:"/docker" | inurl:"/registry" | inurl:"/repo" | inurl:"/repository" | inurl:"/docker-registry"',
+        },
+      ],
+  },
+  {
+      id: "18",
+      title: "Exposed Kubernetes Clusters",
+      description: "Find publicly accessible Kubernetes clusters",
+      category: "BROWSER",
+      dorks: [
+        {
+          id: "18-1",
+          label: "Kubernetes Clusters",
+          query: 'site:(site) inurl:"/kubernetes" | inurl:"/kube" | inurl:"/k8s" | inurl:"/cluster" | inurl:"/api/v1"',
+        },
+      ],
+  },
+  {
+      id: "19",
+      title: "ENV Files",
+      description: "Find exposed ENV files",
+      category: "BROWSER",
+      dorks: [
+        {
+          id: "19-1",
+          label: "ENV Files",
+          query: 'site:(site) filename:.env | filename:.env.local | ext:env | ext:dotenv | ext:envrc | ext:env.example | ext:env.sample | ext:env.bak | ext:env.backup',
+        },
+      ],
+  },
+  {
+      id: "20",
+      title: "Exposed API Endpoints",
+      description: "Find publicly accessible API endpoints",
+      category: "BROWSER",
+      dorks: [
+        {
+          id: "20-1",
+          label: "API Endpoints",
+          query: 'site:(site) inurl:"/api" | inurl:"/v1" | inurl:"/v2" | inurl:"/v3" | inurl:"/graphql" | inurl:"/rest" | inurl:"/json" | inurl:"/xml"',
+        },
+      ],
+  },
+  {
+      id: "21",
+      title: "HTaccess Files Or Info",
+      description: "Find exposed HTaccess files or information",
+      category: "BROWSER",
+      dorks: [
+        {
+          id: "21-1",
+          label: "HTaccess Files",
+          query: 'site:(site) filename:.htaccess | filename:.htpasswd | inurl:"/.htaccess" | inurl:"/.htpasswd"',
+        },
+      ],
+  },
+  {
+      id: "22",
+      title: "Install Files and Setups",
+      description: "Find exposed install files and setup pages",
+      category: "BROWSER",
+      dorks: [
+        {
+          id: "22-1",
+          label: "Install Files",
+          query: 'site:(site) inurl:"/install" | inurl:"/setup" | inurl:"/config" | inurl:"/configuration" | inurl:"/init" | inurl:"/initialize" | inurl:"/db_init" | inurl:"/db_setup" | inurl:"/db_install"',
+        },
+      ],
   }
 ]
